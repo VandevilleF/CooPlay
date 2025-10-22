6a5bfda7a3f51e173ebb8913d083c4d0db13f796
VandevilleF <flo.vandeville@gmail.com>
2025-10-22 14:28:40 +0200

    feat: add getEncryptionKey method to retrieve encryption key for events

--- frontend/src/services/chatService.js
+++ frontend/src/services/chatService.js
@@ -1,6 +1,12 @@
 import httpClient from './httpClient';
 
 export const chatService = {
+	// Récupère la clé d'encryptage
+	getEncryptionKey: async (eventId) => {
+		const response = await httpClient.get(`/chat/events/${eventId}/encryption_key`);
+		return response.data.data.key;
+	},
+
 	// Récupère les messages d'un événement
 	getEventMessages: async (eventId) => {
 		try {
@@ -24,8 +30,8 @@ export const chatService = {
 	},
 
 	// Envoi un message
-	sendMessage: async (eventId, message) => {
-		const response = await httpClient.post(`/chat/events/${eventId}/messages`, { message });
-		return response.data;
-	}
+	// sendMessage: async (eventId, message) => {
+	// 	const response = await httpClient.post(`/chat/events/${eventId}/messages`, { message });
+	// 	return response.data;
+	// }
 }
