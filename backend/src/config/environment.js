b8234d2f2471bc6a951d37b51333f36c10d2da0e
VandevilleF <flo.vandeville@gmail.com>
2025-10-22 14:22:41 +0200

    fix: add masterKey to config in environment.js

--- backend/src/config/environment.js
+++ backend/src/config/environment.js
@@ -5,5 +5,6 @@ export const config = {
   port: process.env.PORT || 5000,
   env: process.env.NODE_ENV || 'development',
   dbUrl: process.env.DATABASE_URL,
-  redisUrl: process.env.REDIS_URL
+  redisUrl: process.env.REDIS_URL,
+  masterKey: process.env.MASTER_ENCRYPTION_KEY,
 };
